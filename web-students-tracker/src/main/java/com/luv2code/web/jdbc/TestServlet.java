d95b1f97a032aa76367576e48f99cc33f40e5475
yukkiAsunaJr <thanh.putdut@gmail.com>
2021-05-22 02:35:14 +0700

    22-5

--- web-students-tracker/src/main/java/com/luv2code/web/jdbc/TestServlet.java
+++ web-students-tracker/src/main/java/com/luv2code/web/jdbc/TestServlet.java
@@ -20,6 +20,7 @@ import javax.sql.DataSource;
 public class TestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	// Define datasource/connection pool
+	
 	@Resource(name = "jdbc/web_student_tracker")
 	private DataSource dataSource;
 
